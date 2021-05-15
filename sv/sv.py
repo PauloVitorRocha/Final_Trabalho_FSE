8c936543b81d8af81ec480134341f9ac8d74d278
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-05-14 22:59:23 -0300

    tratando comodo com acento e com espaços

--- sv/sv.py
+++ sv/sv.py
@@ -1,10 +1,38 @@
+from PyQt5 import QtWidgets
 import paho.mqtt.client as mqtt
 import json
+import unidecode
+# from PyQt5 import *
+# from PyQt5.QtWidgets import QApplication, QMainWindow
+# import sys
+
+
+
+# class RAPAZ:
+#     def window(self):
+#         app = QApplication(sys.argv)
+#         win = QMainWindow()
+#         win.setGeometry(200, 200, 300, 300)
+#         win.setWindowTitle("TITULO JANELA")
+#         label = QtWidgets.QLabel(win)
+#         label.setText("My label")
+#         label.move(50, 50)
+#         win.show()
+#         sys.exit(app.exec_())
+
+#     def comunicacaoMQTT(self):
+        
+
+
+# if __name__ == '__main__':
+#     RAPAZ.window()
 
 client = mqtt.Client()
 client.connect("test.mosquitto.org", 1883)
 
 stringBase = "fse2020/170062465/"
+espList = {}
+
 
 def on_connect(client, userdata, flags, rc):
     print("connected to broker")
@@ -12,19 +40,29 @@ def on_connect(client, userdata, flags, rc):
     # client.subscribe('fse2020/170062465/estado')
     
 def on_message(client, userdata, message):
-    print(message.topic)
+    # print(message.topic)
     topic = message.topic
+    #se for cadastrar novo dispositivo
+    if("fse2020/170062465/dispositivos/" not in (topic)):
+        for key, value in espList.items():
+            print("key = ",key)
+            print("value = ",value)
+
     if("fse2020/170062465/dispositivos/" in (topic)):
-        print(json.loads(message.payload.decode())['id'])
         id = json.loads(message.payload.decode())['id']
-        print("Vamos cadastrar jaja {id}".format(id=id))
-        comodo = input('Onde você gostaria de cadastra-lo?')
-        obj={}
-        obj['comodo']=comodo
-        jsonComodo = json.dumps(obj)
-        client.unsubscribe('fse2020/170062465/dispositivos/#')
-        client.publish(topic, jsonComodo)
-        client.subscribe('fse2020/170062465/dispositivos/#')
+        if(id in espList):
+            print("ESP ja cadastrada")
+        else:
+            comodo = input('Onde você gostaria de cadastrar o novo equipamento?')
+            comodo = unidecode.unidecode(comodo).replace(' ','').lower()
+            msg={}
+            msg['comodo']=comodo
+            jsonComodo = json.dumps(msg)
+            client.unsubscribe('fse2020/170062465/dispositivos/#')
+            client.publish(topic, jsonComodo)
+            novoTopico = stringBase+comodo + '/temperatura'
+            espList[id] = novoTopico
+            client.subscribe(novoTopico)
 
 
 while True:
